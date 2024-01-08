bca9c312fa8d3790ca1fca545157161369a44557
jaehyeong305 <jaehyeong.ha.305@gmail.com>
2024-01-08 12:33:15 +0900

    add component: search component

---
+++ src/app/components/search/Search.tsx
@@ -0,0 +1,11 @@
+import styles from "./search.module.css";
+
+const Search: React.FC = () => {
+    return (
+        <div className={styles.SearchWrapper}>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default Search;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 import Header from './components/header/Header'
+import Search from './components/search/Search'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header/>
+      <Search/>
     </main>
   )
 }
