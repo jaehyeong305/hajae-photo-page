c477ed12461c7f3beff23c2cb92cbcc62f2ad530
jaehyeong305 <jaehyeong.ha.305@gmail.com>
2024-01-11 21:16:45 +0900

    add bookmark component

---
+++ src/app/bookmark/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+import Header from "../components/header/Header";
+
+const Bookmark: React.FC = () => {
+    return (
+        <div className={styles.BookmarkWrapper}>
+            <Header />
+            
+        </div>
+    )
+}
+
+export default Bookmark;
